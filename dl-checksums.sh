5007384b11fda99c5bdf08f9eba2d0c5fe024824
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-06-26 23:47:49 -0400

    initial commit

---
+++ dl-checksums.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/concourse/concourse/releases/download
+readonly APP=concourse
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tgz}
+    local platform="${os}-${arch}"
+    local url=$MIRROR/v${ver}/${APP}-${ver}-${platform}.${archive_type}.sha1
+    printf "    # %s\n"  $url
+    printf "    %s: sha1:%s\n" $platform $(curl -SsL $url | awk '{print $1}')
+}
+
+dl_ver () {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver windows amd64 zip
+}
+
+dl_ver ${1:-6.3.0}
